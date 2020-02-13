4e492745f36218481aaf56e0e883153afba3c0e8
Fiona Ochs <fiona.oaks@gmail.com>
2020-02-13 13:34:54 -0800

    callback

--- photo-gallery/src/App.js
+++ photo-gallery/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Creature from './ImageItem.js';
 import Header from './Header.js';
 import ImageList from './ImageList.js';
+import Select from './Select.js';
 // import request from 'superagent';
 
 
@@ -13,18 +14,17 @@ export default class App extends Component {
       horns: null,
       keyword: null
     };
-    handleChange = e => {
-      this.setState({selected: e.target.value});
-    }
-    handleHorns = e => {
-      this.setState({horns: Number(e.target.value)});
-    }
-    handleKeyword = e => {
-      this.setState({keyword: e.target.value});
-    }
     
-
     render () {
+      const handleHorns = e => {
+        this.setState({horns: Number(e.target.value)});
+      }
+      const handleKeyword = e => {
+        this.setState({keyword: e.target.value});
+      }
+      const handleChange = e => {
+        this.setState({selected: e.target.value});
+      }
 
       const { selected } = this.state;
 
@@ -51,34 +51,9 @@ export default class App extends Component {
         
         return (
           <div className="App">
-      <Header></Header>
-      <select className="horned-types" onChange={this.handleChange}>
-                <option value="" defaultValue>
-                    All Horned Types
-                </option>
-                <option value="narwhal">Narwhal</option>
-                <option value="rhino">Rhino</option>
-                <option value="unicorn">Unicorn</option>
-                <option value="unilego">Unilego</option>
-                <option value="triceratops">Triceratops</option>
-                <option value="markhor">Markhor</option>
-                <option value="mouflon">Mouflon</option>
-                <option value="addax">Addax</option>
-                <option value="chameleon">Chameleon</option>
-                <option value="lizard">Lizard</option>
-                <option value="dragon">Dragon</option>
-            </select>
-            <select className="horned-types" onChange={this.handleHorns}>
-                <option value="" defaultValue>
-                    Number of Horns
-                </option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="100">100</option>
-
-            </select>
-            <input type="text" className="form" onChange={this.handleKeyword}></input>
+          <Header></Header>
+          <Select handleChangeCallback={handleChange} handleHornsCallback={handleHorns}/>
+          <input type="text" className="form" onChange={handleKeyword}></input>
           <ImageList creaturesNodes={creaturesNodes}></ImageList>
           </div>
       );
